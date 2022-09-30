a1c09545fc87d0e593957438e280eb94af01dc7f
luis <araujodasilvaluisgustavo@gmail.com>
2022-09-29 21:38:38 -0300

    atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produto/cadastrar-produto.component';
+import { ListarProdutoComponent } from './components/produtos/listar-produto/listar-produto.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'produtos', component:ListarProdutoComponent},
+  {path:'produtos/cadastrar', component:CadastrarProdutoComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
